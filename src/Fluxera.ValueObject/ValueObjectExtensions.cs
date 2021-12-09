bd02e4f601b0fbc3cc02dc78a3801fc5253fe861
Matthias Gernand <mgernand@users.noreply.github.com>
2021-12-10 00:16:22 +0100

    Added extension method to check if a type is a value object. (#4)

---
+++ src/Fluxera.ValueObject/ValueObjectExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace Fluxera.ValueObject
+{
+	using System;
+	using JetBrains.Annotations;
+
+	[PublicAPI]
+	public static class ValueObjectExtensions
+	{
+		/// <summary>
+		///		Checks if the given type is a <see cref="ValueObject{TValueObject}" />.
+		/// </summary>
+		/// <param name="type">The type to check.</param>
+		/// <returns><c>true</c> if the type is a value object; <c>false</c> otherwise.</returns>
+		public static bool IsValueObject(this Type type)
+		{
+			try
+			{
+				bool isSubclassOf = type.IsSubclassOfRawGeneric(typeof(ValueObject<>));
+				return isSubclassOf && !type.IsInterface && !type.IsAbstract;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		private static bool IsSubclassOfRawGeneric(this Type toCheck, Type generic)
+		{
+			while (toCheck != null && toCheck != typeof(object))
+			{
+				Type cur = toCheck.IsGenericType ? toCheck.GetGenericTypeDefinition() : toCheck;
+				if (generic == cur)
+				{
+					return true;
+				}
+
+				toCheck = toCheck.BaseType;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ tests/Fluxera.ValueObject.UnitTests/ValueObjectExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Fluxera.ValueObject.UnitTests
+{
+	using System;
+	using FluentAssertions;
+	using Model;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class ValueObjectExtensionsTests
+	{
+		[Test]
+		public void ShouldReturnTrueForValueObjectType()
+		{
+			Type type = typeof(Country);
+			type.IsValueObject().Should().BeTrue();
+		}
+
+		[Test]
+		public void ShouldReturnTrueForValueObjectDerivedType()
+		{
+			Type type = typeof(GermanBankAccount);
+			type.IsValueObject().Should().BeTrue();
+		}
+
+		[Test]
+		public void ShouldReturnFalseForObjectType()
+		{
+			Type type = typeof(object);
+			type.IsValueObject().Should().BeFalse();
+		}
+	}
+}
