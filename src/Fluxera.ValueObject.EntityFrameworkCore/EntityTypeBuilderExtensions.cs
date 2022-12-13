b70bf382233720e6937b2d2b3cbe9ae7b8a2e640
Matthias Gernand <mgernand@users.noreply.github.com>
2022-12-13 11:15:24 +0100

    Moved value object configuration for EFCore to the entity builder level. (#31)

---
+++ src/Fluxera.ValueObject.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace Fluxera.ValueObject.EntityFrameworkCore
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
+	using Fluxera.Guards;
+	using JetBrains.Annotations;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+	using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+	/// <summary>
+	///     Extension methods for the <see cref="ModelBuilder" /> type.
+	/// </summary>
+	[PublicAPI]
+	public static class EntityTypeBuilderExtensions
+	{
+		/// <summary>
+		///     Configure the <see cref="EntityTypeBuilder" /> to use the
+		///     <see cref="PrimitiveValueObjectConverter{TValueObject,TValue}" />.
+		/// </summary>
+		/// <param name="entityTypeBuilder"></param>
+		public static void UsePrimitiveValueObject(this EntityTypeBuilder entityTypeBuilder)
+		{
+			Guard.Against.Null(entityTypeBuilder);
+
+			IEnumerable<PropertyInfo> properties = entityTypeBuilder.Metadata
+				.ClrType
+				.GetProperties()
+				.Where(type => type.PropertyType.IsPrimitiveValueObject());
+
+			foreach(PropertyInfo property in properties)
+			{
+				Type enumerationType = property.PropertyType;
+				Type valueType = enumerationType.GetPrimitiveValueObjectValueType();
+
+				Type converterTypeTemplate = typeof(PrimitiveValueObjectConverter<,>);
+
+				Type converterType = converterTypeTemplate.MakeGenericType(enumerationType, valueType);
+
+				ValueConverter converter = (ValueConverter)Activator.CreateInstance(converterType);
+
+				entityTypeBuilder
+					.Property(property.Name)
+					.HasConversion(converter);
+			}
+		}
+	}
+}
