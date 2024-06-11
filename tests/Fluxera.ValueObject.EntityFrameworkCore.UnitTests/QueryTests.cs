823f48a0ea1f1a20cb280422febdb36298522fde
Matthias Gernand <matthias.gernand@fluxera.com>
2024-06-11 22:02:27 +0200

    Added an integration test for queries using a primitive value object in the filter for EFCore.

---
+++ tests/Fluxera.ValueObject.EntityFrameworkCore.UnitTests/QueryTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Fluxera.ValueObject.EntityFrameworkCore.UnitTests
+{
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using FluentAssertions;
+	using Fluxera.ValueObject.EntityFrameworkCore.UnitTests.Model;
+	using Microsoft.EntityFrameworkCore;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class QueryTests
+	{
+#pragma warning disable NUnit1032
+		private TestDbContext context;
+#pragma warning restore NUnit1032
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.context = DbContextFactory.Generate(1);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			this.context?.Dispose();
+		}
+
+		[Test]
+		public async Task ShouldFindByPrimitiveValueObject()
+		{
+			Person linqFilterResult = await this.context
+				.Set<Person>()
+				.Where(x => x.Age.Value < 40)
+				.FirstOrDefaultAsync();
+
+			linqFilterResult.Should().NotBeNull();
+		}
+	}
+}
--- tests/Fluxera.ValueObject.EntityFrameworkCore.UnitTests/TestDbContext.cs
+++ tests/Fluxera.ValueObject.EntityFrameworkCore.UnitTests/TestDbContext.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using Fluxera.ValueObject.EntityFrameworkCore.UnitTests.Model;
 	using Microsoft.EntityFrameworkCore;
 	using Microsoft.EntityFrameworkCore.Infrastructure;
 	using Microsoft.EntityFrameworkCore.Metadata.Builders;
